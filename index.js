c3033a8daf817c679c7f23f224869d556fcac8b2
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-05-26 23:32:11 +0300

    add files

---
+++ index.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("form1");
+// const inputText = document.getElementById("input");
+// const input = document.getElementById("submit")
+const ol = document.getElementById("list")
+
+form.addEventListener("submit",function(e){
+
+    if(e.target.input.value === ""){
+        alert("your input is empty")
+ }
+ else {
+     let list = document.createElement("li");
+     list.textContent = e.target.input.value;
+     list.classList.add("list-group-item","category");
+     ol.appendChild(list)
+    
+ } 
+ e.target.input.value=""
+save()
+
+})
+
+list.addEventListener("click" ,function(e){
+    e.target.remove()
+   
+
+    
+} )
+
+
+
+
+
+function save() {
+    localStorage.setItem("data", ol.innerHTML);
+}
+function read() {
+  ol.innerHTML=localStorage.getItem("data")
+}
+read()
